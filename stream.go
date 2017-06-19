5e6997210f230d46109a52e29bb1cb9117c25f3e
Luke Granger-Brown <git@lukegb.com>
2017-06-20 00:36:32 +0100

    Remove package doc from stream.go
    If it's specified repeatedly, it'll appear repeatedly in e.g. godoc.org/github.com/rakyll/portmidi.
    
    Remove the copy in stream.go to let portmidi.go be canonical.

--- stream.go
+++ stream.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package portmidi provides portmidi bindings.
 package portmidi
 
 // #cgo LDFLAGS: -lportmidi
